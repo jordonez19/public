4a378d5edc1d502a7d9e8307aeb075542b30e4f1
xavi <javier19pedraza@gmail.com>
2024-03-07 18:26:04 -0500

    nury

--- components/Team/index.js
+++ components/Team/index.js
@@ -7,33 +7,33 @@ const Team = () => {
       <div className="team section" id="team">
         <div className="container">
           <div className="row ">
-            <div className="col-lg-6 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="team-member">
                 <div className="main-content">
-                  <img src="team/pilar.jpg" alt="" />
+                  <img style={{ boxShadow: '0 0 5px #cecece' }} src="team/pilar.jpg" alt="" />
                   <span className="category">Gerente</span>
                   <h4>Pilar Rojas</h4>
                 </div>
               </div>
             </div>
-            <div className="col-lg-6 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="team-member">
                 <div className="main-content">
-                  <img src="team/gonzalo.jpg" alt="" />
+                  <img style={{ boxShadow: '0 0 5px #cecece' }} src="team/gonzalo.jpg" alt="" />
                   <span className="category">Guia profesional</span>
                   <h4>Gonzalo Tolosa</h4>
                 </div>
               </div>
             </div>
-            {/* <div className="col-lg-6 col-md-6">
+            <div className="col-lg-4 col-md-6">
               <div className="team-member">
                 <div className="main-content">
-                  <img src="assets/images/member-02.jpg" alt="" />
+                  <img style={{ boxShadow: '0 0 5px #cecece' }} src="team/nury.jpg" alt="" />
                   <span className="category">Agente de Viajes</span>
-                  <h4>Nury Buitrago</h4>
+                  <h4>Nury </h4>
                 </div>
               </div>
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
